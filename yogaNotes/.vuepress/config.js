40078527b1c8a94e6f279bfa32c74872d9737358
roiff <roiff@126.com>
2023-10-09 03:18:43 +0800

    更新版本

---
+++ yogaNotes/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports ={
+  //这里面路径最开始的/是指向.vuepress/public/的
+  base:'/',//部署站点的基础路径,默认/
+  lang: 'zh-CN',
+  title: '晓晓的笔记',
+  // description: '这是我的第一个 VuePress 站点',//站点描述，它会被每个页面的 Frontmatter 中的 description 字段覆盖
+  // head: [['link', { rel: 'icon', href: '/images/logo.PNG' }]],//站点头部的icon
+  plugins: ['autobar'],
+  themeConfig: {
+    nav:[
+        { text: '首页', link: '/' },
+    ],
+    sidebar: 'auto'
+},
+}
