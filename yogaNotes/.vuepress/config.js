a689074f2a663187d8adbc0c248116c5f5f7938f
roiff <roiff@126.com>
2023-10-09 04:13:17 +0800

    bug

--- yogaNotes/.vuepress/config.js
+++ yogaNotes/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports ={
   //这里面路径最开始的/是指向.vuepress/public/的
-  base:'/',//部署站点的基础路径,默认/
+  base:'/yogaNotes',//部署站点的基础路径,默认/
   lang: 'zh-CN',
   title: '晓晓的笔记',
   // description: '这是我的第一个 VuePress 站点',//站点描述，它会被每个页面的 Frontmatter 中的 description 字段覆盖
